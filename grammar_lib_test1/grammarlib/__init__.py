b0df6669120f0a4712b253e25da76db655370b23
TingshanPan <48140155+TingshanPan@users.noreply.github.com>
2022-11-16 03:10:26 -0500

    setup

--- grammar_lib_test1/grammarlib/__init__.py
+++ grammar_lib_test1/grammarlib/__init__.py
@@ -2,4 +2,4 @@ from grammarlib.happy_text_to_text import HappyTextToText, TTSettings
 from grammarlib.tt.trainer import TTTrainArgs, TTEvalArgs
 
 
-name = "happytransformer"
+name = "grammarlib"
--- grammar_lib_test1/setup.py
+++ grammar_lib_test1/setup.py
@@ -7,17 +7,17 @@ current_location = pathlib.Path(__file__).parent
 readme = (current_location / "README.md").read_text()
 
 setup(
-    name = 'happytransformer',
+    name = 'grammarlib',
     packages = find_packages(),
-    version = '2.4.1',
+    version = '1.0.0',
     license='Apache 2.0',
-    description = "Happy Transformer is an API built on top of Hugging Face's Transformer library that makes it easy to utilize state-of-the-art NLP models.",
+    description = ".",
     long_description= readme,
     long_description_content_type='text/markdown',
-    author = "The Happy Transformer Development Team",
-    author_email = 'happytransformer@gmail.com',
-    url = 'https://github.com/EricFillion/happy-transformer',
-    keywords = ['bert', 'roberta', 'xlnet', "transformer", "happy", "HappyTransformer",  "classification",  "nlp", "nlu", "natural", "language", "processing", "understanding"],
+    author = "The AI Grammar Check Development Team",
+    author_email = 'tingshan1114@gmail.com',
+    url = 'https://github.com/TingshanPan/grammar_check_lib',
+    keywords = ['bert', 'roberta', 'xlnet', "transformer",  "nlp", "nlu", "natural", "language", "processing", "grammar"],
 
     install_requires=[
             'torch>=1.0',
@@ -35,6 +35,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "Topic :: Text Processing :: Linguistic",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
 
