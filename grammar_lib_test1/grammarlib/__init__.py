9c19207d87c9e51c3e3779bcd8f54fd96beea3b5
TingshanPan <48140155+TingshanPan@users.noreply.github.com>
2022-11-16 03:18:02 -0500

    test1-2

--- grammar_lib_test1/grammarlib/__init__.py
+++ grammar_lib_test1/grammarlib/__init__.py
@@ -1,5 +1,4 @@
 from grammarlib.happy_text_to_text import HappyTextToText, TTSettings
-from grammarlib.tt.trainer import TTTrainArgs, TTEvalArgs
-
+from grammarlib.tt.trainer import TTEvalArgs, TTTrainArgs
 
 name = "grammarlib"
